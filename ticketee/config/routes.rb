133e5cc801c36f29b200fcdd71a30f0d6d296f1b
Bontgoy <tyjkeating@gmail.com>
2015-09-20 20:19:48 -0500

    add admin projects controller.  edit test broken

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
 
-  namespace :admin do
-  	root 'application#index'
+  resources :projects, only: [:index, :show, :edit, :update] do
+  	resources :tickets
   end
 
-  devise_for :users
-  resources :projects do
-  	resources :tickets
+   namespace :admin do
+  	root 'application#index'
+  	resources :projects, only: [:new, :create, :destroy]
   end
 
   root 'projects#index'
